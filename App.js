b56f7891ab8dba50d55914d997df68ae97faaf73
Vương Phù Sanh Hoàng Thiện <vuongpsht51197@gmail.com>
2019-07-17 23:59:03 +0700

    ahihi

--- App.js
+++ App.js
@@ -5,47 +5,122 @@
  * @format
  * @flow
  */
-
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+// ví dụ khi t đang ghi như thế này
+//thằng git sẽ lắng nghe thay đổi
+//muốn xem thử khác nhau gữa trước và sau khi dùng câu lệnh
+//git add .
+//thì làm như này
+//git status
+//bây h test cái cho nóng
+// quên phải save nhé ^^
+//đó nó thấy liền
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  WebView,
+  SafeAreaView,
+  TouchableWithoutFeedback
+} from "react-native";
 
 export default class ABC extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			counter: 0
-		}
-	}
-	increment = () => {
-		//code here
-		this.setState({
-			counter: this.state.counter++
-		})
-	}
-	// ví dụ khi t đang ghi như thế này
-	//thằng git sẽ lắng nghe thay đổi
-	//muốn xem thử khác nhau gữa trước và sau khi dùng câu lệnh
-	//git add .
-	//thì làm như này 
-	//git status
-	//bây h test cái cho nóng
-	// quên phải save nhé ^^ 
-	//đó nó thấy liền 
-	render() {
-		return (
-			<View>
-				<Text>{this.state.counter}</Text>
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      //link gốc
+      link: "https://bitboard.bit-corp.work/",
+      // init giá trị của cangoback
+      canGoBack: false
+    };
+  }
+  increment = () => {
+    //code here
+    this.setState({
+      counter: this.state.counter++
+    });
+  };
 
-				<Button
-					title="click me"
-					onPress={() => { this.increment() }}
-				/>
-				<WebView
-					source={{ uri: 'https://bitboard.bit-corp.work/' }}
-				/>
+  // khi có sự kiện thay đổi sẽ gọi đến hàm này
+  handleStateChange = wvState => {
+    //kiểm tra thông tin của ws
+    console.log(wsState);
+    //kiểm tra wsState có gì
+    this.setState({
+      canGoBack: wvState.canGoBack
+    });
+  };
+  //xử lý sự kiện goback
+  goBack = () => {
+    //kiểm tra có goback đc hay không
+    if (this.state.canGoBack) {
+      //nếu được thì goback
+      this.webview.goBack();
+    }
+  };
+  render() {
+    console.log(this.state.canGoBack);
+    return (
+      //canh lề trên dưới cho iphoneX
+      <SafeAreaView style={styles.container}>
+        {/* <Text>{this.state.counter}</Text>
 
-			</View>
+        <Button
+          title="click me"
+          onPress={() => {
+            this.increment();
+          }}
+        /> */}
 
-		)
-	}
+        {/* Header chứa button goback */}
+        <View style={styles.header}>
+          {/* xử lý sự kiện khi click goback */}
+          <TouchableWithoutFeedback onPress={this.goBack}>
+            <Text
+              // check xem có thể goback hay không
+              // nếu có thì dùng styles.active
+              // nếu không thì dùng styles.nonActive
+              style={this.state.canGoBack ? styles.active : styles.nonActive}
+            >
+              GoBack
+            </Text>
+          </TouchableWithoutFeedback>
+        </View>
+        <WebView
+          //khai báo ref cho webview
+          ref={c => (this.webview = c)}
+          // lắng nghe thay đổi khi có sự kiện di chuyển
+          onNavigationStateChange={wvState => this.handleStateChange(wvState)}
+          //link cho webview
+          source={{ uri: this.state.link }}
+        />
+      </SafeAreaView>
+    );
+  }
 }
+
+// khai báo 1 biến styles để gọi lại
+// dễ quản lý code
+// code đẹp hơn
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    height: 70,
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 20,
+    justifyContent: "space-between"
+  },
+  active: {
+    color: "blue"
+  },
+  nonActive: {
+    color: "grey"
+  }
+});
