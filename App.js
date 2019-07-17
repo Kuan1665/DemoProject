3f2bbd857d20099abe37a62ffd79c6c19a3027d0
グエン　ヴー <>
2019-07-18 01:00:06 +0900

    test git

--- App.js
+++ App.js
@@ -10,33 +10,42 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 
 export default class ABC extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            counter: 0
-        }
-    }
-    increment = () => {
-        //code here
-        this.setState({
-            counter: this.state.counter++
-        })
-    }
-    render() {
-        return (
-            <View>
-                <Text>{this.state.counter}</Text>
+	constructor(props) {
+		super(props)
+		this.state = {
+			counter: 0
+		}
+	}
+	increment = () => {
+		//code here
+		this.setState({
+			counter: this.state.counter++
+		})
+	}
+	// ví dụ khi t đang ghi như thế này
+	//thằng git sẽ lắng nghe thay đổi
+	//muốn xem thử khác nhau gữa trước và sau khi dùng câu lệnh
+	//git add .
+	//thì làm như này 
+	//git status
+	//bây h test cái cho nóng
+	// quên phải save nhé ^^ 
+	//đó nó thấy liền 
+	render() {
+		return (
+			<View>
+				<Text>{this.state.counter}</Text>
 
-                <Button
-                    title="click me"
-                    onPress={() => { this.increment() }}
-                />
-                <WebView
-                    source={{ uri: 'https://bitboard.bit-corp.work/' }}
-                />
+				<Button
+					title="click me"
+					onPress={() => { this.increment() }}
+				/>
+				<WebView
+					source={{ uri: 'https://bitboard.bit-corp.work/' }}
+				/>
 
-            </View>
+			</View>
 
-        )
-    }
+		)
+	}
 }
