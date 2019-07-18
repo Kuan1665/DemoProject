a13e73a3f6575a80fb3ef7b3e1dad1a4fb85aeb8
Vương Phù Sanh Hoàng Thiện <vuongpsht51197@gmail.com>
2019-07-18 23:07:42 +0700

    ok

--- App.js
+++ App.js
@@ -34,20 +34,21 @@ export default class ABC extends Component {
       //link gốc
       link: "https://bitboard.bit-corp.work/",
       // init giá trị của cangoback
-      canGoBack: false
+      canGoBack: false,
+      page: 1
     };
   }
   increment = () => {
     //code here
     this.setState({
-      counter: this.state.counter++
+      counter: this.state.counter + 1
     });
   };
 
   // khi có sự kiện thay đổi sẽ gọi đến hàm này
   handleStateChange = wvState => {
     //kiểm tra thông tin của ws
-    console.log(wsState);
+    console.log(wvState);
     //kiểm tra wsState có gì
     this.setState({
       canGoBack: wvState.canGoBack
@@ -55,48 +56,70 @@ export default class ABC extends Component {
   };
   //xử lý sự kiện goback
   goBack = () => {
+    console.log(this.webview);
     //kiểm tra có goback đc hay không
     if (this.state.canGoBack) {
       //nếu được thì goback
       this.webview.goBack();
     }
   };
+  change = () => {
+    if (this.state.page === 1) {
+      this.setState({
+        page: 2
+      });
+    } else {
+      this.setState({
+        page: 1
+      });
+    }
+  };
   render() {
-    console.log(this.state.canGoBack);
+    console.log(this.state.counter);
     return (
       //canh lề trên dưới cho iphoneX
       <SafeAreaView style={styles.container}>
-        {/* <Text>{this.state.counter}</Text>
-
-        <Button
-          title="click me"
-          onPress={() => {
-            this.increment();
-          }}
-        /> */}
-
-        {/* Header chứa button goback */}
-        <View style={styles.header}>
-          {/* xử lý sự kiện khi click goback */}
-          <TouchableWithoutFeedback onPress={this.goBack}>
-            <Text
-              // check xem có thể goback hay không
-              // nếu có thì dùng styles.active
-              // nếu không thì dùng styles.nonActive
-              style={this.state.canGoBack ? styles.active : styles.nonActive}
-            >
-              GoBack
-            </Text>
-          </TouchableWithoutFeedback>
-        </View>
-        <WebView
-          //khai báo ref cho webview
-          ref={c => (this.webview = c)}
-          // lắng nghe thay đổi khi có sự kiện di chuyển
-          onNavigationStateChange={wvState => this.handleStateChange(wvState)}
-          //link cho webview
-          source={{ uri: this.state.link }}
-        />
+        {this.state.page === 1 ? (
+          <View>
+            <Text>{this.state.counter}</Text>
+            <Button
+              title="click me"
+              onPress={() => {
+                this.increment();
+              }}
+            />
+            <Button onPress={this.change} title="web view" />
+          </View>
+        ) : (
+          <View style={{ flex: 1 }}>
+            <View style={styles.header}>
+              {/* xử lý sự kiện khi click goback */}
+              <TouchableWithoutFeedback onPress={this.goBack}>
+                <Text
+                  // check xem có thể goback hay không
+                  // nếu có thì dùng styles.active
+                  // nếu không thì dùng styles.nonActive
+                  style={
+                    this.state.canGoBack ? styles.active : styles.nonActive
+                  }
+                >
+                  GoBack
+                </Text>
+              </TouchableWithoutFeedback>
+              <Button onPress={this.change} title="counter" />
+            </View>
+            <WebView
+              //khai báo ref cho webview
+              ref={c => (this.webview = c)}
+              // lắng nghe thay đổi khi có sự kiện di chuyển
+              onNavigationStateChange={wvState =>
+                this.handleStateChange(wvState)
+              }
+              //link cho webview
+              source={{ uri: this.state.link }}
+            />
+          </View>
+        )}
       </SafeAreaView>
     );
   }
@@ -114,8 +137,7 @@ const styles = StyleSheet.create({
     width: "100%",
     flexDirection: "row",
     alignItems: "center",
-    marginHorizontal: 20,
-    justifyContent: "space-between"
+    marginHorizontal: 20
   },
   active: {
     color: "blue"
